d5dac8a1e1c89c27bb833a26a090c9bcafbce2b8
Ben Bolker <bbolker@gmail.com>
2016-03-29 22:49:52 -0400

    update fill tests

--- assignments/project_fill_tests.py
+++ assignments/project_fill_tests.py
@@ -1,6 +1,15 @@
 import numpy as np
+import random
 ## change this line to use your macid
-from bolker_proj_fill import *  
+from bolker_proj_fill import *
+## different random number choice methods give different results
+## rand_method = "default"  ## numpy.random.randint, numpy.random.choice
+## rand_method = "choice_repl_False"
+rand_method = "rand_sample"
+if rand_method!="rand_sample":
+    seed = np.random.seed
+else:
+    seed = random.seed
 print("all tests should result in True ...")
 a = np.array([0,0,1,1,1,0,0,0,1,0,0,1,0])
 print("test fillable (interior, r=default(1)):")
@@ -29,10 +38,13 @@ b = a.copy()
 print("strict test fill_one, seed=101")
 ## can't be absolutely sure that filling algorithm will use exactly
 ## 1 random number, so stream may not be identical?
-np.random.seed(101)
+seed(101)
 fill_one(b)
 c = a.copy()
-c[6] = 1
+if rand_method=="default":
+    c[6] = 1
+elif rand_method=="rand_sample":
+    c[0] = 1
 print(all(b==c))
 print("strict test fill_one: only one legal choice")
 ## there is only one legally fillable space (e[99]),
@@ -46,19 +58,25 @@ f[99] = 1
 print(np.all(e==f))
 print("loose test fill_one, seed=101")
 print(np.sum(b)==np.sum(a)+1)
-np.random.seed(101)
+seed(101)
 d = fill_sim(10)
 print("loose test fill_sim(10), seed=101")
 print(not any_fillable(d))
 print("strict test fill_sim(10), seed=101")
-print(all(d==np.array([0,1,0,0,1,0,1,0,0,1])))
-np.random.seed(101)
+if rand_method=="default":
+    print(all(d==np.array([0,1,0,0,1,0,1,0,0,1])))
+elif rand_method=="rand_sample":
+    print(all(d==np.array([ 1, 0, 0, 1, 0, 1, 0, 1, 0, 1])))
+seed(101)
 print("loose test fill_sim(10), seed=101")
 print(not any_fillable(d))
 print("strict test fill_sim(6,2), seed=101")
-np.random.seed(101)
+seed(101)
 e = fill_sim(6,2)
-print(all(e==np.array([1,0,0,1,0,0])))
+if rand_method=="default":
+    print(all(e==np.array([1,0,0,1,0,0])))
+elif rand_method=="rand_sample":
+    print(all(e==np.array([0,1,0,0,1,0])))
 print("loose test fill_sim(6,2), seed=101")
 print(not any_fillable(e,2))
 
