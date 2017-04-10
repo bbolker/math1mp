552293557890e4bd0470cb543be39d6dab126fca
Ben Bolker <bbolker@gmail.com>
2017-04-10 12:21:09 -0400

    tweak index (old exam notes)

--- code/roman5.py
+++ code/roman5.py
@@ -1,12 +1,12 @@
 def roman_to_int(r):
     """convert roman to integer: step through string"""
-    v = 0
+    v = 0   ## initialize counter
     d = dict(I=1,V=5,X=10,L=50,C=100,D=500,M=1000)
     ## iterate by index so we can check whether we are
     ##  on the last value or not
     for i in range(len(r)):
-        val = d[r[i]]
-        if i<(len(r)-1):
+        val = d[r[i]]  ## dictionary lookup
+        if i<(len(r)-1):  ## if not quite at end of string
             val_next = d[r[i+1]]
             if val_next > val:
                 v -= val
