4050ec66598dcebd65b2c9bca5c47418cecf851c
Ben Bolker <bbolker@gmail.com>
2019-09-16 15:18:54 -0400

    push week 1, 2 junk

---
+++ code/week1_stuff.py
@@ -0,0 +1,99 @@
+"hello"
+
+x = "hello"
+precipitation_per_day = 123.4
+precipitation per day = 123.4
+precipitationPerDay
+precipitationperday
+PRECIPITATIONPERDAY
+
+precip_day
+p = 1    
+p = 23.4 ## float
+p = 23.0
+p = "twenty three"
+p = False
+p
+print(p)
+p = 23.4 ## float
+p * 2
+type(p)
+
+x=3
+y=3.0
+z="a"
+q=complex(1,2)
+type(int(x+y))
+int(23.56)
+
+True and True
+
+True and True
+
+a = "because"
+a.rev()
+
+a[3]
+a[0]
+a[100]
+len(a)
+a[-1]
+a[0:3]  ## select elements 0, 1, and 2
+a[3:0]
+a[100]
+a[100:101]
+a[2:101]
+a[:]
+a[2:]
+a[2:len(a)]
+a[2:len(a):2]
+a[::-1]
+str(12)
+type(12)
+x = 12
+x[1:2]
+int(str(x)[1])
+name = "asdfasdfasf"
+name[5]
+
+
+x = [1,2,3]
+print(x)
+y = [3,1,2]
+print(y)
+
+y[1]
+type(y)
+type(y[1])
+
+y = [2, "a", True, 45.0]
+type(y[2])
+z = "abc"
+z[2]
+z[1:]
+type(y[2:4])
+y[2:3]
+y[2]
+
+z = [1,2,[3,4,5]]
+z[3]
+z[2]
+zz = z[2]
+zz[2]
+z[2][2]
+z[1][2]
+
+user = ["Ben Bolker",  "HH 314", "prof", 1000000000000]
+
+[1,2,3,4,5,6,7,8,9,10]
+range(10)
+list(range(10))
+list(range(0,10,3))
+x = [1,2,3]
+x.extend(5)
+x.extend([5])
+print(x)
+x.append(5)
+print(x)
+
+x += [4,5]
---
+++ code/week2_stuff.py
@@ -0,0 +1,35 @@
+if False:
+    print("no")
+if True:
+    print("yes")
+    print("something else")
+    
+print("yes")
+print("something else")
+
+x=2
+if (x<=0):
+    print("what??")
+elif(x==1):
+    print("one")
+elif(x==2):
+    print("two")
+else:
+    print("many")
+    
+def date_fashion(you, date):
+  if you <=2 or date <= 2:
+    result = 0
+  elif you >= 8 or date >=8:
+    result = 2
+  else:
+    result = 1
+  return(result)
+
+while True:
+    print("hello")
+    
+x = 0
+while True:
+    x= x+1
+    print(x)
